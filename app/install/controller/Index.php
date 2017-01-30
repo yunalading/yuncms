762febab11c9d8a503f08c6f6b0cfd1aec7fca8d
2898117012 <2898117012@qq.com>
2017-01-30 23:46:08 +0800

    安装步骤step1页面制作

--- app/install/controller/Index.php
+++ app/install/controller/Index.php
@@ -16,4 +16,16 @@ class Index extends InstallBaseController {
     public function index() {
         return view();
     }
+    public function step1(){
+        return view();
+    }
+    public function step2(){
+        return view();
+    }
+    public function step3(){
+        return view();
+    }
+    public function step4(){
+        return view();
+    }
 }
