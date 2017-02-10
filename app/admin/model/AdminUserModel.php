b0e2787fc7676ccb6c638317bd951424cdf9272e
httgo <68527761@qq.com>
2017-02-10 14:40:55 +0800

    test

---
+++ app/admin/model/AdminUserModel.php
@@ -0,0 +1,33 @@
+<?php
+// +----------------------------------------------------------------------
+// | YunCMS
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2016 http://www.yunalading.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: chenqianhao <68527761@qq.com>
+// +----------------------------------------------------------------------
+
+namespace app\admin\model;
+use app\common\model\BaseModel;
+use think\Db;
+class AdminUserModel extends BaseModel
+{
+    protected $tableName='admin_user';
+    /**
+     * 获取配置列表信息
+     * @Author [chenqianhao] <68527761@qq.com>
+     */
+    public function getAllConfig()
+    {
+        return Db::name($this->tableName)->find();
+    }
+
+
+
+
+
+
+
+}
