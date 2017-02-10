c049a9d89f09fe7ac8c622093943e59a37f03527
jabber <2898117012@qq.com>
2017-02-10 17:34:14 +0800

    jump页面脚本

---
+++ frontend/common/js/jump-message.js
@@ -0,0 +1,18 @@
+'use strict';
+var $ = require('jquery');
+var timer = 10;
+function forwardTimer() {
+    if (timer > 0) {
+        $('#system-message').find('#wait').html(timer);
+        timer--;
+        setTimeout(forwardTimer, 1000);
+    } else {
+        location.href = $('#system-message').find('#href').attr('href');
+    }
+}
+$(function () {
+   if($('#system-message').find('#wait').length > 0){
+       timer = parseInt($('#system-message').find('#wait').html());
+       forwardTimer();
+   }
+});
