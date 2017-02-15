d09a78b1b09b570b08c8bc0b13054bd2ddcfc4af
jabber <2898117012@qq.com>
2017-02-15 22:14:11 +0800

    管理中心界面

--- app/extra/authorization.php
+++ app/extra/authorization.php
@@ -21,53 +21,156 @@ return [
                 ]
             ]
         ], [
-            'id' => 'fdsa',
+            'id' => 'content',
             'name' => '内容',
             'submenus' => [
                 [
-                    'id' => 'welcome',
-                    'name' => '欢迎页面',
-                    'submenus' => [
-                        [
-                            'id' => 'welcome',
-                            'name' => '欢迎页面',
-                            'href' => '/admin/index/welcome'
-                        ]
-                    ]
-                ], [
-                    'id' => 'welcome',
-                    'name' => '欢迎页面',
-                    'href' => '/admin/index/welcome'
+                    'id' => '',
+                    'name' => '内容列表',
+                    'href' => ''
                 ]
             ]
         ], [
-            'id'=>'',
-            'name'=>'用户',
-        ],[
-            'id' => '',
-            'name' => '专题',
-        ], [
-            'id' => '',
-            'name' => '分类',
-        ], [
-            'id' => '',
-            'name' => '模型',
-        ], [
-            'id' => '',
-            'name' => '标签',
-        ],[
-            'id'=>'',
-            'name'=>'留言',
-        ],[
-            'id'=>'',
-            'name'=>'广告',
-        ],[
-            'id'=>'',
-            'name'=>'系统',
-            'submenus'=>[
+            'id' => 'user',
+            'name' => '用户',
+            'submenus' => [
                 [
-                    'id'=>'',
-                    'name'=>'导航',
+                    'id' => '',
+                    'name' => '用户列表',
+                    'href' => ''
+                ]
+            ]
+        ], [
+            'id' => 'subject',
+            'name' => '专题',
+            'submenus' => [
+                [
+                    'id' => '',
+                    'name' => '专题列表',
+                    'href' => ''
+                ]
+            ]
+        ], [
+            'id' => 'category',
+            'name' => '分类',
+            'submenus' => [
+                [
+                    'id' => '',
+                    'name' => '分类列表',
+                    'href' => ''
+                ]
+            ]
+        ], [
+            'id' => 'model',
+            'name' => '模型',
+            'submenus' => [
+                [
+                    'id' => '',
+                    'name' => '模型列表',
+                    'href' => ''
+                ]
+            ]
+        ], [
+            'id' => 'tag',
+            'name' => '标签',
+            'submenus' => [
+                [
+                    'id' => '',
+                    'name' => '标签列表',
+                    'href' => ''
+                ]
+            ]
+        ], [
+            'id' => 'comment',
+            'name' => '留言',
+            'submenus' => [
+                [
+                    'id' => '',
+                    'name' => '留言列表',
+                    'href' => ''
+                ]
+            ]
+        ], [
+            'id' => 'ads',
+            'name' => '广告',
+            'submenus' => [
+                [
+                    'id' => '',
+                    'name' => '普通广告',
+                    'href' => ''
+                ], [
+                    'id' => '',
+                    'name' => '脚本广告',
+                    'href' => ''
+                ], [
+                    'id' => '',
+                    'name' => '幻灯片',
+                    'href' => ''
+                ]
+            ]
+        ], [
+            'id' => 'system',
+            'name' => '系统',
+            'submenus' => [
+                [
+                    'id' => '',
+                    'name' => '基本信息',
+                ], [
+                    'id' => 'nav',
+                    'name' => '导航管理',
+                ], [
+                    'id' => '',
+                    'name' => '邮件配置',
+                ], [
+                    'id' => '',
+                    'name' => '短信配置',
+                ], [
+                    'id' => '',
+                    'name' => '集成登录',
+                    'submenus' => [
+                        [
+                            'id' => '',
+                            'name' => 'QQ',
+                            'href' => ''
+                        ], [
+                            'id' => '',
+                            'name' => '微信',
+                            'href' => ''
+                        ], [
+                            'id' => '',
+                            'name' => '新浪微博',
+                            'href' => ''
+                        ]
+                    ]
+                ], [
+                    'id' => '',
+                    'name' => '权限管理',
+                    'submenus' => [
+                        [
+                            'id' => '',
+                            'name' => '管理员',
+                            'href' => ''
+                        ], [
+                            'id' => '',
+                            'name' => '角色',
+                            'href' => ''
+                        ]
+                    ]
+                ], [
+                    'id' => '',
+                    'name' => '地区管理',
+                ], [
+                    'id' => '',
+                    'name' => '图片管理',
+                ], [
+                    'id' => '',
+                    'name' => '上传设置',
+                ], [
+                    'id' => '',
+                    'name' => '常量管理',
+                ], [
+                    'id' => '',
+                    'name' => '友情链接',
                 ]
             ]
         ]
