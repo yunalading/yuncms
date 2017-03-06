b5887038d91bff820a97702e72a185a48894322e
jabber <2898117012@qq.com>
2017-03-06 11:39:35 +0800

    消息提示

--- frontend/admin/js/admin.js
+++ frontend/admin/js/admin.js
@@ -1,4 +1,5 @@
 'use strict';
 var $ = require('jquery');
+require('../../common/js/jump-message');
 require('../../common/js/captcha');
 require('./manager-box');
--- public/static/admin/js/admin.js
+++ public/static/admin/js/admin.js
@@ -1,13 +1,14 @@
-/*! yuncms v1.0.0 | by yunalading Team | http://www.yunalading.com | (c) 2017 HTTGO, Inc. |  | 2017-03-05"P"14:03:26 UTC */ 
+/*! yuncms v1.0.0 | by yunalading Team | http://www.yunalading.com | (c) 2017 HTTGO, Inc. |  | 2017-03-06"A"03:03:02 UTC */ 
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (global){
 'use strict';
 var $ = (typeof window !== "undefined" ? window['jQuery'] : typeof global !== "undefined" ? global['jQuery'] : null);
+require('../../common/js/jump-message');
 require('../../common/js/captcha');
 require('./manager-box');
 
 }).call(this,typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{"../../common/js/captcha":3,"./manager-box":2}],2:[function(require,module,exports){
+},{"../../common/js/captcha":3,"../../common/js/jump-message":4,"./manager-box":2}],2:[function(require,module,exports){
 (function (global){
 'use strict';
 var $ = (typeof window !== "undefined" ? window['jQuery'] : typeof global !== "undefined" ? global['jQuery'] : null);
@@ -25,6 +26,28 @@ $(function () {
     $('#captcha').click(function () {
        $(this).attr('src',$(this).attr('src'));
     });
+});
+
+}).call(this,typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{}],4:[function(require,module,exports){
+(function (global){
+'use strict';
+var $ = (typeof window !== "undefined" ? window['jQuery'] : typeof global !== "undefined" ? global['jQuery'] : null);
+var timer = 10;
+function forwardTimer() {
+    if (timer > 0) {
+        $('#system-message').find('#wait').html(timer);
+        timer--;
+        setTimeout(forwardTimer, 1000);
+    } else {
+        location.href = $('#system-message').find('#href').attr('href');
+    }
+}
+$(function () {
+    if ($('#system-message').find('#wait').length > 0) {
+        timer = parseInt($('#system-message').find('#wait').html());
+        forwardTimer();
+    }
 });
 
 }).call(this,typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
