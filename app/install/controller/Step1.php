ec32b78c9410cbd9c1d239fc478510a0f0a779c5
chenqianhao <68527761@qq.com>
2017-03-07 18:00:56 +0800

    安装步骤1

--- app/install/controller/Step1.php
+++ app/install/controller/Step1.php
@@ -6,12 +6,16 @@
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
-// | Author: jabber <2898117012@qq.com>
+// | Author: chenqianhao <68527761@qq.com>
 // +----------------------------------------------------------------------
 
 
 namespace app\install\controller;
 
+use app\core\system\check\env\GdCheck;
+use app\core\system\check\env\OsCheck;
+use think\Log;
+use app\core\system\BaseCheck;
 
 /**
  * Class Complete
@@ -22,6 +26,13 @@ class Step1 extends InstallWizard {
      * @return \think\response\View
      */
     public function index() {
+        $env_check = [
+            'os' => new OsCheck(),
+//            'gd' => new GdCheck(),
+        ];
+//        echo '<pre>';
+//        print_r($env_check);exit;
+        Log::info("aaaa");
         return view();
     }
 }
