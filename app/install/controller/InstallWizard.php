7b7a47e82042ac6b6cb1294f149f5d8244f78627
jabber <2898117012@qq.com>
2017-03-08 13:27:31 +0800

    格式化

--- app/install/controller/InstallWizard.php
+++ app/install/controller/InstallWizard.php
@@ -11,16 +11,16 @@
 
 
 namespace app\install\controller;
+
 use app\core\Install;
+
 /**
  * 安装向导
  * Class InstallWizard
  * @package app\install\controller
  */
-abstract class InstallWizard extends InstallBaseController
-{
-    public function __construct()
-    {
+abstract class InstallWizard extends InstallBaseController {
+    public function __construct() {
         parent::__construct();
         //检查是否安装
         $this->checkInstall();
@@ -29,8 +29,7 @@ abstract class InstallWizard extends InstallBaseController
     /**
      * 检查是否安装，已安装提示
      */
-    protected function checkInstall()
-    {
+    protected function checkInstall() {
         if (Install::checkInstall()) {
             $this->success('安装成功', url('/'), '系统已安装，可正常使用！');
         }
