b5529b3dd2e54d494c5dfa1a559a85c9c4d63ce3
jabber <2898117012@qq.com>
2017-03-09 22:08:13 +0800

    修改授权页面

--- frontend/admin/js/role-check-box.js
+++ frontend/admin/js/role-check-box.js
@@ -1,10 +1,7 @@
 'use strict';
 var $ = require('jquery');
 $(function () {
-    $('.role-check-access .am-panel-hd').find('.am-ucheck-checkbox').change(function () {
-        $(this).parents('.role-nav').find('.am-panel-bd .am-ucheck-checkbox').uCheck('check');
-    });
     $('.role-check-access .menu-title').find('.am-ucheck-checkbox').change(function () {
-        //todo 子选择没做
+        //todo 按下层引导上层选择
     });
 });
