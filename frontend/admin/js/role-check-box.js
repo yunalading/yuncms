6aee179a65c260169960a773e6c6e9e489cd0a93
jabber <2898117012@qq.com>
2017-03-10 18:04:42 +0800

    权限选择

--- frontend/admin/js/role-check-box.js
+++ frontend/admin/js/role-check-box.js
@@ -1,7 +1,25 @@
 'use strict';
 var $ = require('jquery');
 $(function () {
-    $('.role-check-access .menu-title').find('.am-ucheck-checkbox').change(function () {
-        //todo 按下层引导上层选择
+    $('.am-panel-bd .am-ucheck-checkbox').change(function () {
+        if ($(this).is(':checked')) {
+            //向上添加
+            $(this).parents('.am-panel').find('.am-panel-hd .am-ucheck-checkbox').uCheck('check');
+            $(this).parents('.node-1').find('.node-1-title .am-ucheck-checkbox').uCheck('check');
+            $(this).parents('.node-2').find('.node-2-title .am-ucheck-checkbox').uCheck('check');
+        } else {
+            //向下取消
+            if ($(this).parent('label').is('.node-1-title')) {
+                $(this).parents('.node-1').find('.node-2 .am-ucheck-checkbox').uCheck('uncheck');
+            }
+            if ($(this).parent('label').is('.node-2-title')) {
+                $(this).parents('.node-2').find('div .am-ucheck-checkbox').uCheck('uncheck');
+            }
+        }
+    });
+    $('.am-panel-hd .am-ucheck-checkbox').change(function () {
+        if (!$(this).is(':checked')) {
+            $(this).parents('.am-panel').find('.am-panel-bd .am-ucheck-checkbox').uCheck('uncheck');
+        }
     });
 });
