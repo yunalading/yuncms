01c5f9cc3d7f60602485927ad29fa05686de94bf
jabber <2898117012@qq.com>
2017-03-21 17:59:20 +0800

    添加管理员

--- frontend/admin/js/global.js
+++ frontend/admin/js/global.js
@@ -1,9 +1,5 @@
 'use strict';
 var $ = require('jquery');
 $(function () {
-    $('.checkbox-switch').bootstrapSwitch({
-        size: 'xs',
-        onText: "启用",
-        offText: "停用"
-    });
+
 });
