3b562640cc790903c8f26281213000ef9bfff381
jabber <2898117012@qq.com>
2017-03-24 09:29:54 +0800

    留言

--- app/admin/controller/Guestbook.php
+++ app/admin/controller/Guestbook.php
@@ -30,4 +30,37 @@ class Guestbook extends AdminBaseController {
         $this->assign('page', $page);
         return view();
     }
+
+    /**
+     * 查看留言
+     * @return \think\response\View
+     */
+    public function viewer() {
+        if (!empty($this->param) && $this->param['id']) {
+            //编辑页面初始化数据
+            $guestbook = GuestBookModel::get($this->param['id']);
+            $this->assign('guestbook', $guestbook);
+        }
+        return view();
+    }
+
+    /**
+     * 删除留言
+     */
+    public function remove() {
+        if (!empty($this->param) && $this->param['id']) {
+            try {
+                if (GuestBookModel::destroy($this->param['id'])) {
+                    $this->success('删除成功!');
+                } else {
+                    $this->error('删除失败!');
+                }
+            } catch (PDOException $e) {
+                Log::error($e);
+                $this->error('删除失败!');
+            }
+        } else {
+            $this->error('参数错误!');
+        }
+    }
 }
