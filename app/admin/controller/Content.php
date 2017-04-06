b2b5ce7c637e3ede927bf03b551cd257b94545b8
chenqianhao <68527761@qq.com>
2017-04-06 15:35:19 +0800

    Merge branch 'develop' of https://github.com/yunalading/yuncms into develop
    # Conflicts:
    #	app/admin/view/upload/index.html
    #	public/static/admin/css/admin.min.css
    #	public/static/admin/css/admin.min.css.map
    #	public/static/admin/js/admin.min.js.map
    #	public/static/core/css/core.min.css
    #	public/static/home/default/css/home.min.css
    #	public/static/install/css/install.min.css
    #	public/static/install/css/install.min.css.map

--- app/admin/controller/Content.php
+++ app/admin/controller/Content.php
@@ -11,7 +11,10 @@
 
 
 namespace app\admin\controller;
+use app\admin\model\CategoryModel;
 use app\admin\model\ContentModel;
+use app\admin\model\ModelModel;
+use app\admin\validate\ContentValidate;
 
 /**
  * Class Content
@@ -32,41 +35,62 @@ class Content extends AdminBaseController
         $this->assign('page', $page);
         return view();
     }
-
-    /**
-     * @return \think\response\View
-     */
-    public function add()
-    {
-        return view();
-    }
-
     /**
      * @return \think\response\View
      */
     public function edit()
     {
+        $action_name = '添加';
+        $models = new CategoryModel();
+        $cate = $models->putCateOut();
+        $this->assign('cate', $cate);
+        $model = new ContentModel();
+        if ($this->request->isPost()) {
+            //验证数据
+            $param = array_filter($this->post['content']);
+            $Validate = new ContentValidate();
+            if (!$Validate->check($param, [], 'update')) {
+                $this->error($Validate->getError());
+            }
+            if (isset($this->param['id']) && $this->param['id']) {
+                $action_name = '修改';
+                $where['content_id'] = $this->param['id'];
+                $model->update($param, $where);
+            } else {
+                $param['push_time'] = time();
+                $model->create($param);
+            }
+            $this->success($action_name . '成功!', url('/admin/content'));
+        } else {
+            if (!empty($this->param) && $this->param['id']) {
+                $content = ContentModel::get($this->param['id']);
+                $this->assign('content', $content);
+                $action_name = '编辑';
+            }
+        }
+        $this->assign('action_name', $action_name);
         return view();
     }
 
     /**
      * @return \think\response\View
      */
-    public function delete()
-    {
-        return view();
+    public function delete(){
+        $model = new ContentModel();
+        if (!empty($this->param) && $this->param['id']) {
+            try {
+                if ($model::destroy($this->param['id'])) {
+                    $this->success('删除成功!');
+                } else {
+                    $this->error('删除失败!');
+                }
+            } catch (PDOException $e) {
+                Log::error($e);
+                $this->error('删除失败!');
+            }
+        } else {
+            $this->error('参数错误!');
+        }
     }
 
-    /**
-     * @return \think\response\View
-     */
-    public function trash()
-    {
-        return view();
-    }
-
-    public function editor()
-    {
-        return view();
-    }
 }
---
+++ app/common/validate/BaseContentValidate.php
@@ -0,0 +1,26 @@
+<?php
+// +----------------------------------------------------------------------
+// | YunCMS
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2016 http://www.yunalading.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: chenqianhao <68527761@qq.com>
+// +----------------------------------------------------------------------
+
+
+namespace app\common\validate;
+
+/**
+ * Class BaseConstantValidate
+ * @package app\common\validate
+ */
+class BaseContentValidate extends BaseValidate {
+    protected $rule = [
+
+    ];
+    protected $message = [
+
+    ];
+}
