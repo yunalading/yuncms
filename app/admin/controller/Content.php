d57c4032b4fae7c2bc658fffe5da1de645e7525f
chenqianhao <68527761@qq.com>
2017-04-06 13:29:19 +0800

    Merge branch 'develop' of https://github.com/yunalading/yuncms into develop
    # Conflicts:
    #	app/admin/view/upload/index.html
    #	public/static/admin/css/admin.min.css
    #	public/static/admin/css/admin.min.css.map
    #	public/static/admin/js/admin.min.js.map
    #	public/static/core/css/core.min.css
    #	public/static/home/default/css/home.min.css
    #	public/static/install/css/install.min.css
    #	public/static/install/css/install.min.css.map

--- app/admin/controller/Content.php
+++ app/admin/controller/Content.php
@@ -6,52 +6,67 @@
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
-// | Author: jabber <2898117012@qq.com>
+// | Author: chenqianhao <68527761@qq.com>
 // +----------------------------------------------------------------------
 
 
 namespace app\admin\controller;
+use app\admin\model\ContentModel;
 
 /**
  * Class Content
  * @package app\admin\controller
  */
-class Content extends AdminBaseController {
+class Content extends AdminBaseController
+{
+
     /**
      * @return \think\response\View
      */
-    public function index() {
+    public function index()
+    {
+        $model = new ContentModel();
+        $list = $model->paginate();
+        $page = $list->render();
+        $this->assign('list', $list);
+        $this->assign('page', $page);
         return view();
     }
 
     /**
      * @return \think\response\View
      */
-    public function add() {
+    public function add()
+    {
         return view();
     }
 
     /**
      * @return \think\response\View
      */
-    public function edit() {
+    public function edit()
+    {
         return view();
     }
 
     /**
      * @return \think\response\View
      */
-    public function delete() {
+    public function delete()
+    {
         return view();
     }
 
     /**
      * @return \think\response\View
      */
-    public function trash() {
+    public function trash()
+    {
         return view();
     }
-    public function editor() {
+
+    public function editor()
+    {
         return view();
     }
 }
