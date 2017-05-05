5303771504317ff8caae09659c8023394d14f2a8
chenqianhao <68527761@qq.com>
2017-05-05 13:40:29 +0800

    前台模型

---
+++ app/home/model/LinkModel.php
@@ -0,0 +1,24 @@
+<?php
+// +----------------------------------------------------------------------
+// | YunCMS
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2016 http://www.yunalading.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: chenqianhao <68527761@qq.com>
+// +----------------------------------------------------------------------
+
+
+namespace app\home\model;
+
+
+use app\common\model\BaseLinkModel;
+
+/**
+ * Class LinkModel
+ * @package app\admin\model
+ */
+class LinkModel extends BaseLinkModel {
+
+}
