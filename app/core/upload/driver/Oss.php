a04703ea5bce9f54d582368547dbcfb530d80cde
chenqianhao <68527761@qq.com>
2017-05-08 15:00:49 +0800

    文件上传

--- app/core/upload/driver/Oss.php
+++ app/core/upload/driver/Oss.php
@@ -38,11 +38,11 @@ class Oss extends Upload
         //实例化OSS
         $ossClient=new OssClient($newConf['KeyId'],$newConf['KeySecret'],$newConf['Endpoint']);
         $res = (object)array(); //申明一个空对象返回
-        //try{
+        try{
             $size = ceil($newConf['maxSize']*1048576);
             //$info = $file->validate(['size' => $newConf['maxSize'], 'ext' => $newConf['allowExts']])->move(ROOT_PATH . 'public' . DS . 'data' . DS . 'upload' .DS. 'images' .DS. 'oss');
             $info = $file->validate(['size' => $size, 'ext' => $newConf['allowExts']])->move(ROOT_PATH . 'public' . DS . $newConf['savePath']);
-            if (!$info) {
+            if (!is_object($info)) {
                 $res->info=$file->getError();
                 $res->code=0;
                 $res->msg=$file->getError();
@@ -56,14 +56,14 @@ class Oss extends Upload
             $res->info=$oss_info;//oss-request-url
             $res->code=1;
             $res->msg="上传成功!";
-        //} catch(OssException $e) {
+        } catch(OssException $e) {
             //如果出错这里返回报错信息
             //return $e;
-//            $res->info=$e->getMessage();
-//            $res->code=0;
-//            $res->msg=$e->getMessage();
-//            return $res;
-//        }
+            $res->info=$e->getMessage();
+            $res->code=0;
+            $res->msg=$e->getMessage();
+            return $res;
+        }
         return $res;
     }
 
--- app/core/upload/driver/Server.php
+++ app/core/upload/driver/Server.php
@@ -34,27 +34,27 @@ class Server extends Upload
         }
         $newConf = array_merge(config('upload.image'), $conf);
         $res = (object)array(); //申明一个空对象返回
-        //try{
+        try{
             $size = ceil($newConf['maxSize']*1048576);
             $info = $file->validate(['size' => $size, 'ext' => $newConf['allowExts']])->move(ROOT_PATH . 'public' . DS . $newConf['savePath']);
-            if (!$info) {
+            if (!is_object($info)) {
                 $res->info=$file->getError();
                 $res->code=0;
                 $res->msg="验证失败!";
-                return $res;
+            }else{
+                $res->pathUrl =  $newConf['savePath'] .DS. $info->getSaveName();
+                $res->info=$info;
+                $res->code=1;
+                $res->msg="上传成功!";
             }
-            $res->pathUrl =  $newConf['savePath'] .DS. $info->getSaveName();
-            $res->info=$info;
-            $res->code=1;
-            $res->msg="上传成功!";
-//        } catch(\Exception $e) {
-//            //如果出错这里返回报错信息
-//            //return $e;
-//            $res->info=$e->getMessage();
-//            $res->code=0;
-//            $res->msg="上传失败!";
-//            return $res;
-//        }
+        } catch(\Exception $e) {
+            //如果出错这里返回报错信息
+            //return $e;
+            $res->info=$e->getMessage();
+            $res->code=0;
+            $res->msg="上传失败!";
+            return $res;
+        }
         return $res;
     }
 }
